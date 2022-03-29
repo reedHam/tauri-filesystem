9230a3c9c9d0760beb180d8e3c87ccfffff523ed
Ganfar <devrh@protonmail.com>
2022-03-29 16:19:33 -0700

    test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn hello_world() -> String {
+    return "hello world".to_string();
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![hello_world])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import type { Component } from 'solid-js';
+import { Component, lazy } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+
+import { invoke } from '@tauri-apps/api/tauri'
+
+const HelloP = lazy(async () => {
+  const str = await invoke('hello_world');
+
+  return {
+    default: < div >
+      {str as string}
+    </div> as Component,
+  }
+});
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+          <HelloP />
       </header>
     </div>
   );
